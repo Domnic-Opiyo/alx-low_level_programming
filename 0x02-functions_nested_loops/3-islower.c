f492e703edfa0c87aaa73c0d2c1cfc228304aee0
Domnic-Opiyo <reagandomnque@gmail.com>
2023-07-18 11:15:56 -0700

    ./4-isalpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lower case
+ * Return: always 0
+ * @c: parinthesis
+ */
+int _islower(int c)
+{
+
+	if (c <= 99 && c >= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  _isalpha - checks for upper and lowercase alphabets
+ *  @c: variable
+ *  Return: always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) && (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
